8e6f3cadbbef91d757d97eb985c2c92cc207408a
mayssaGH <mayssagnm@gmail.com>
2026-02-09 19:29:20 +0100

    feat: enable tilling ground and planting crops using their corresponding tools
    - add tilled dirt v2 tileset
    - implement field cursor component for adding tilled ground cells
    - implement crops cursor component for adding crops cells
    - add new test scene

---
+++ scenes/components/crops_cursor_component.gd
@@ -0,0 +1,59 @@
+extends Node
+class_name CropsCursorComponent
+
+@export var tilled_soil_tilemap_layer: TileMapLayer
+
+@onready var player: Player = get_tree().get_first_node_in_group("player")
+
+var corn_plant_scene = preload("res://scenes/objects/plants/corn.tscn")
+var tomato_plant_scene = preload("res://scenes/objects/plants/tomato.tscn")
+
+var mouse_position: Vector2
+var cell_position: Vector2i
+var cell_source_id: int
+var local_cell_position: Vector2
+var distance: float
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("remove_dirt"):
+		if ToolManager.selected_tool == DataTypes.Tools.TillGround:
+			get_cell_under_mouse()
+			remove_crop()
+	elif event.is_action_pressed("hit"):
+		if ToolManager.selected_tool == DataTypes.Tools.PlantCorn or ToolManager.selected_tool == DataTypes.Tools.PlantTomato:
+			get_cell_under_mouse()
+			add_crop()
+
+
+func get_cell_under_mouse() -> void:
+	mouse_position = tilled_soil_tilemap_layer.get_local_mouse_position()
+	cell_position = tilled_soil_tilemap_layer.local_to_map(mouse_position)
+	cell_source_id = tilled_soil_tilemap_layer.get_cell_source_id(cell_position)
+	local_cell_position = tilled_soil_tilemap_layer.map_to_local(cell_position)
+	distance = player.global_position.distance_to(local_cell_position)
+
+func has_same_position(node: Node) -> bool:
+	return node.global_position == local_cell_position
+
+func get_crop_node_index() -> int:
+	return get_parent().find_child("CropFields").get_children().find_custom(has_same_position)
+
+func add_crop() -> void:
+	# only add crop node if the distance is less than 20,
+	# the cell is on a tilled soil tilemap, and the cell is not already occupied
+	if distance < 20.0 && cell_source_id != -1 && get_crop_node_index() == -1:
+		if ToolManager.selected_tool == DataTypes.Tools.PlantCorn:
+			var corn_instance = corn_plant_scene.instantiate() as Node2D
+			corn_instance.global_position = local_cell_position
+			get_parent().find_child("CropFields").add_child(corn_instance)
+		if ToolManager.selected_tool == DataTypes.Tools.PlantTomato:
+			var tomato_instance = tomato_plant_scene.instantiate() as Node2D
+			tomato_instance.global_position = local_cell_position
+			get_parent().find_child("CropFields").add_child(tomato_instance)
+
+func remove_crop() -> void:
+	if distance < 20.0 && cell_source_id != -1:
+		var crop_node_index = get_crop_node_index()
+		if crop_node_index != -1:
+			var node = get_parent().find_child("CropFields").get_child(crop_node_index)
+			node.queue_free()
---
+++ scenes/components/field_cursor_component.gd
@@ -0,0 +1,51 @@
+extends Node
+class_name FieldCursorComponent
+
+@export var grass_tilemap_layer: TileMapLayer
+@export var tilled_soil_tilemap_layer: TileMapLayer
+@export var terrain_set: int = 0
+@export var terrain: int = 1
+
+@onready var player: Player = get_tree().get_first_node_in_group("player")
+
+var mouse_position: Vector2
+var cell_position: Vector2i
+var cell_source_id: int
+var local_cell_position: Vector2
+var distance: float
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("remove_dirt"):
+		if ToolManager.selected_tool == DataTypes.Tools.TillGround:
+			get_cell_under_mouse()
+			remove_tilled_soil_cell()
+	elif event.is_action_pressed("hit"):
+		if ToolManager.selected_tool == DataTypes.Tools.TillGround:
+			get_cell_under_mouse()
+			add_tilled_soil_cell()
+
+func _get_mouse_world() -> Vector2:
+	var vp := get_viewport()
+	var mouse_screen: Vector2 = vp.get_mouse_position()
+	return vp.get_canvas_transform().affine_inverse() * mouse_screen
+
+func _cell_on_layer(layer: TileMapLayer, mouse_world: Vector2) -> Vector2i:
+	var mouse_local: Vector2 = layer.to_local(mouse_world)
+	return layer.local_to_map(mouse_local)
+
+func get_cell_under_mouse() -> void:
+	var mouse_world: Vector2 = _get_mouse_world()
+	cell_position = _cell_on_layer(tilled_soil_tilemap_layer, mouse_world)
+	var cell_on_grass: Vector2i = _cell_on_layer(grass_tilemap_layer, mouse_world)
+	cell_source_id = grass_tilemap_layer.get_cell_source_id(cell_on_grass)
+	local_cell_position = tilled_soil_tilemap_layer.map_to_local(cell_position)
+	var cell_center_world: Vector2 = tilled_soil_tilemap_layer.to_global(local_cell_position)
+	distance = player.global_position.distance_to(cell_center_world)
+	
+func add_tilled_soil_cell() -> void:
+	if distance < 20.0 && cell_source_id != -1:
+		tilled_soil_tilemap_layer.set_cells_terrain_connect([cell_position], terrain_set, terrain, true)
+
+func remove_tilled_soil_cell() -> void:
+	if distance < 20.0 && cell_source_id != -1:
+		tilled_soil_tilemap_layer.set_cells_terrain_connect([cell_position], 0, -1, true)
